61706ff12d3af33d0c7f7aa4331fe614c293ffb2
Abdul Rehman Liaqat <arl@plista.com>
2017-12-03 02:13:10 +0100

    working

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from cherrypy.lib import static
 import cherrypy
 import os
 from jinja2 import Environment, FileSystemLoader
+from requests.auth import HTTPDigestAuth
+import ast
 
 localDir = os.path.dirname(__file__)
 absDir = os.path.join(os.getcwd(), localDir)
@@ -25,26 +27,37 @@ class Landing_Page(object):
         raise cherrypy.HTTPRedirect("https://simulator-api.db.com/gw/oidc/authorize?response_type=token&redirect_uri=localhost:5000&client_id=49dff74c-4c3d-43dc-bbe5-cf2d8c9bf4e9")
         #return "https://simulator-api.db.com/gw/oidc/authorize?response_type=token&redirect_uri=localhost:5000&client_id=49dff74c-4c3d-43dc-bbe5-cf2d8c9bf4e9"
 
-    def parse_code(parse_token):
-        ind = parse_token.index("access_token")
-        parsed = parse_token.split("&")[0][ind+13:]
-        response =  requests.get("https://simulator-api.db.com/gw/dbapi/v1/ageCertificate?certificationMethod=LEGAL_AGE", headers = {"Authorization":"Bearer "+parsed})
-        if(response.ok):
-            bracket =  str(response.content).index("}")
-        if(str(response.content)[15:bracket] == 'true'):
-            return True
-        elif(str(response.content)[15:bracket] == 'false'):
-            return False
-        else:
-            print("connection broken")
+
 
     @cherrypy.expose
-    def some(self):
-        tmpl = env.get_template('some.html')
-        print(cherrypy.url())
-        print(cherrypy.request)
-        print()
-        return tmpl.render()
+    def some(self, code): 
+        r = requests.post("https://simulator-api.db.com/gw/oidc/token", data={"grant_type":"authorization_code",
+                                                                          "code":code,
+                                                                          "redirect_uri":"http://localhost:5000/some"},
+                                                                           auth=('9afba87e-d902-450d-8942-79d7a5e0b0a3','ANQLk7veOPIrOUlec4uBUA8--6d6R9V-DM4zD95NnikzjJPs3prjJZXIldpW-KwZ-w-x8I1ghQVWJj3jgx884QQ'))
+
+        access_token = ast.literal_eval(r.content.decode())["access_token"]
+        def parse_code(parsed):
+#        ind = parse_token.index("access_token")
+#        parsed = parse_token.split("&")[0][ind+13:]
+            response =  requests.get("https://simulator-api.db.com/gw/dbapi/v1/ageCertificate?certificationMethod=LEGAL_AGE", headers = {"Authorization":"Bearer "+parsed})
+            if(response.ok):
+                bracket =  str(response.content).index("}")
+            if(str(response.content)[15:bracket] == 'true'):
+                return True
+            elif(str(response.content)[15:bracket] == 'false'):
+                return False
+            else:
+                print("connection broken")
+                
+        legal = (parse_code(access_token))
+        if(legal):
+            # The user is legal he can gamble
+            tmpl = env.get_template('some.html')
+            return tmpl.render()
+        else:
+            # Please reach your leagal age
+
 
 config = {
     'global': {
